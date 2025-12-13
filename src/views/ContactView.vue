5cc6a8832836dde657979951281f72a25611a839
Ama8r <hadyh998@gmail.com>
2025-12-13 04:12:31 +0200

    new 1.2

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -296,7 +296,7 @@ const handleSubmit = async () => {
               </div>
             </div>
             <div class="map-container">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55805.67127046134!2d31.06078463387912!3d29.050885762042995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145a25e1064f72af%3A0xbacfa92a1eed60ea!2sBeni%20Suef%2C%20Qism%20Bani%20Sweif%2C%20Beni%20Suef%2C%20Beni%20Suef%20Governorate!5e0!3m2!1sen!2seg!4v1765587592841!5m2!1sen!2seg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            </div>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55805.67127046134!2d31.06078463387912!3d29.050885762042995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145a25e1064f72af%3A0xbacfa92a1eed60ea!2sBeni%20Suef%2C%20Qism%20Bani%20Sweif%2C%20Beni%20Suef%2C%20Beni%20Suef%20Governorate!5e0!3m2!1sen!2seg!4v1765587592841!5m2!1sen!2seg" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            </div>
           </div>
         </div>
       </div>
